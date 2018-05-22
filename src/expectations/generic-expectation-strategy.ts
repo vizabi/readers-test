6b65fbb931cbd1a3243d88a4b886afdbf562bc41
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2018-05-22 18:28:50 +0300

    fix

--- src/expectations/generic-expectation-strategy.ts
+++ src/expectations/generic-expectation-strategy.ts
@@ -1,8 +1,8 @@
 import * as chai from 'chai';
-import { isEmpty, keys, cloneDeep } from 'lodash';
+import {isEmpty, keys, cloneDeep} from 'lodash';
 import * as base64 from 'base-64';
-import { AbstractExpectationStrategy } from './abstract-expectation-strategy';
-import { writeDiff } from './utils';
+import {AbstractExpectationStrategy} from './abstract-expectation-strategy';
+import {writeDiff} from './utils';
 
 const expect = chai.expect;
 
@@ -25,18 +25,20 @@ function stringifyObjectValues(o, isNullAsEmptyString: boolean = false) {
 export class GenericExpectationStrategy extends AbstractExpectationStrategy {
   testIt(err, data, dataSourceSuffix: string, testIndex: number) {
     const fixtureData = require(this.fixturePath.replace(/#datasource#/, dataSourceSuffix));
-    const areEqual = this.equals(data, fixtureData);
     const fixtureDataStr = JSON.stringify(fixtureData, null, 2);
     const dataStr = JSON.stringify(data, null, 2);
 
     // console.log(dataStr);
 
-    expect(!err).to.be.true;
-    expect(data.length).to.equal(fixtureData.length);
     try {
+      expect(!err).to.be.true;
+      expect(data.length).to.equal(fixtureData.length);
+
+      const areEqual = this.equals(data, fixtureData);
+
       expect(areEqual).to.be.true;
     } catch (err) {
-      writeDiff(testIndex, fixtureDataStr, dataStr);
+      writeDiff(JSON.stringify(err, null, 2), testIndex, fixtureDataStr, dataStr);
 
       throw err;
     }
--- src/expectations/utils.ts
+++ src/expectations/utils.ts
@@ -2,11 +2,11 @@ import * as path from "path";
 import * as fs from "fs";
 import * as mkdirp from 'mkdirp';
 
-export function writeDiff(testIndex: number, fixtureDataStr, dataStr) {
+export function writeDiff(error: string, testIndex: number, fixtureDataStr, dataStr) {
   const dir = path.resolve(__dirname, '..', '..', 'test', 'result', `${testIndex}`);
 
   mkdirp.sync(dir);
 
   fs.writeFileSync(path.resolve(dir, 'original.json'), fixtureDataStr);
-  fs.writeFileSync(path.resolve(dir, 'result.json'), dataStr);
+  fs.writeFileSync(path.resolve(dir, 'result.json'), dataStr + error);
 }
